0c985182ac65004c3271e16070dea74bea5651d8
wainoimwangi <wainoi413@gmail.com>
2023-07-20 00:24:56 +0300

    task 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i,j), end=", " if (i != 9 or j != 8) else "\n")
+        if (i != 8 or j != 9):
+            end_char = ", "
+        elif(i == 8 and j == 9):
+            end_char = "\n"
+        print("{:d}{:d}".format(i,j), end=end_char)
