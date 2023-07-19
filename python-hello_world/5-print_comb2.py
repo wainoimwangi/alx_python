3328eeff4841da88557abbe46ac44c75f0e5b2c1
wainoimwangi <wainoi413@gmail.com>
2023-07-19 17:24:49 +0300

    task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    end_char = ", " if i < 99 else ""
-    print("{:02d}".format(i), end=end_char)
+    if (i < 99):
+        end_char = "," 
+    else:
+        end_char = " "
+    print("{:02d}{}".format(i, end_char))
