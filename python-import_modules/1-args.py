92e3c609c326ffc4b9c7dfb9275c8f6afb708929
wainoimwangi <wainoi413@gmail.com>
2023-07-24 00:55:53 +0300

    task 1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+# get number of arguments
+num = len(sys.argv) - 1
+if num == 0:
+    end_char = ". "
+    print("{} arguments".format(num), end=end_char)
+else:
+    end_char = ": "
+    print("{} arguments".format(num), end=end_char)
+for i in range(1, num + 1 ):
+    print("{}: {}".format(i, sys.argv[i]))
