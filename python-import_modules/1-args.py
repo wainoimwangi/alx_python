ebffb62f6a4c5629adb4b0ac9030c75f4831f3f7
wainoimwangi <wainoi413@gmail.com>
2023-07-24 01:00:39 +0300

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,9 +5,12 @@ if __name__ == "__main__":
 num = len(sys.argv) - 1
 if num == 0:
     end_char = ". "
-    print("{} arguments".format(num), end=end_char)
+    print("{} arguments\n".format(num), end=end_char)
+elif num == 1:
+    end_char = ": "
+    print("{} argument\n".format(num), end=end_char)
 else:
     end_char = ": "
-    print("{} arguments".format(num), end=end_char)
+    print("{} arguments\n".format(num), end=end_char)
 for i in range(1, num + 1 ):
-    print("{}: {}".format(i, sys.argv[i]))
+    print("{}: {}".format(i, sys.argv[i]), end="\n")
