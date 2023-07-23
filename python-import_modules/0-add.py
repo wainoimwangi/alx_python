df6b91936d4e5fc6a1bb06bbd1e37ebbce9c9c34
wainoimwangi <wainoi413@gmail.com>
2023-07-23 22:23:38 +0300

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 #import module
 #from add_0 import add
-if __name__ == "__main__":
-    from add_0 import add
 
 #define variables
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a,b)))
 #call the main function
+if __name__ == "__main__":
+    from add_0 import add
+    result = add(a,b)
+    print("{} + {} = {}".format(a, b, result))
