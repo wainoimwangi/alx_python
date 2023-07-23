2ad190c3f84f7ccfcd436421254b888ff7a27c16
wainoimwangi <wainoi413@gmail.com>
2023-07-24 01:21:28 +0300

    task 2

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#import variable
+if __name__ == "__main__":
+    from variable_load_2 import a
+    print("{}".format(a), end="\n")
+    
---
+++ python-import_modules/variable_load_2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+#variable
+a = 1
