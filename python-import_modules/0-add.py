581bccb5f39192c387c02364b3e0242aa3d6cb01
wainoimwangi <wainoi413@gmail.com>
2023-07-23 22:36:24 +0300

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,4 +7,4 @@ b = 2
 if __name__ == "__main__":
     from add_0 import add
     result = add(a, b)
-    print("{} + {} = {}".format(a, b, result), end = "\n")   
+    print("{} + {} = {}".format(a, b, result), end="\n")
