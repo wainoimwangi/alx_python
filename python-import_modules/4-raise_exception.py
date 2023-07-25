c5e2b3e4a979beb76ce247ce7b827d0d0aaaf244
wainoimwangi <wainoi413@gmail.com>
2023-07-25 13:19:05 +0300

    task 5

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -3,11 +3,6 @@
 
 # define raise_exception fucnction
 def raise_exception():
-    # try convert a string to an integer
-    try:
         value = "I love python"
         convert = int(value)
-    # catch the type error te using except
-    except TypeError as e:
-        print("Exception raised", e)
-        raise e
+        return convert
