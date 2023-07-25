b8b878a3d745e48a70d4d6b3e2b6ad5b00171d9a
wainoimwangi <wainoi413@gmail.com>
2023-07-25 11:55:30 +0300

    task 3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# function definition
 
+
+# function definition
 def safe_print_division(a, b):
     # declare a and b variables
     a, b
     # try perform division of two integers
-    try: 
+    try:
         result = a / b
     # if b=0 raise a zerodivisionerror that prints none as a result
     except ZeroDivisionError:
