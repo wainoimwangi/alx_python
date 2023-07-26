f28825299db44675da3b92ccf709cb9e404f8675
wainoimwangi <wainoi413@gmail.com>
2023-07-26 22:53:01 +0300

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -5,13 +5,13 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
-    return
-
-    for row in matrix:
-        for i, element in enumerate(row):
-            if i == len(row) - 1:
-                print("{:d}".format(element))
-            else:
-                print("{:d}".format(element), end=" ")
-        print()
     
+    else:
+        for row in matrix:
+            for i, element in enumerate(row):
+                if i == len(row) - 1:
+                    print("{:d}".format(element))
+                else:
+                    print("{:d}".format(element), end=" ")
+            print()
+        
