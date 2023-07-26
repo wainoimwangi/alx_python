4c3f8652728bca565cb94a7c12bb56f495a1ade1
wainoimwangi <wainoi413@gmail.com>
2023-07-26 23:03:24 +0300

    task 1

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -9,6 +9,7 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element))
+        print()
                 
 
         
