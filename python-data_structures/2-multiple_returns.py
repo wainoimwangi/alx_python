919de3f1346f69a1c8b875cb9d60adcc9acd0962
wainoimwangi <wainoi413@gmail.com>
2023-07-28 12:28:52 +0300

    task 2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,10 +3,9 @@
 
 # function that returns a tuple with the length of a string and its first character.
 def multiple_returns(sentence):
-    sentence = []
     length = len(sentence)
-    first = sentence[[0]]
-    if len(sentence) == 0:
-        sentence[0] == None
+    first = sentence[0]
+    if length == 0:
+        return None
     else:
         return length, first
