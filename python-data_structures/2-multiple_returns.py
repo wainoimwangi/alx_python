c16b3fc175c649bece4be3427fd7d1b471e009c0
wainoimwangi <wainoi413@gmail.com>
2023-07-28 12:29:36 +0300

    task 2

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -6,6 +6,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if length == 0:
-        return None
+        return 0, None
     else:
         return length, first
