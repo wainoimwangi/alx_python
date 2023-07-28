0efab084c9e66c2ef702581c1958916035b062eb
wainoimwangi <wainoi413@gmail.com>
2023-07-28 17:02:12 +0300

    task 2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -2,9 +2,9 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    my_dict = a_dictionary
+    my_dict = a_dictionary[]
     if key in my_dict:
-        my_dict{key} = value
+        my_dict[key] = value
     else:
-        my_dict{key} = value
+        my_dict[key] = value
     return my_dict
