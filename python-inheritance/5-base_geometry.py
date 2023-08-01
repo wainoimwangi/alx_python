365e0348aa8a9fdb567fe60612bef0bb018b2390
wainoimwangi <wainoi413@gmail.com>
2023-08-01 15:11:05 +0300

    task 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -23,6 +23,6 @@ class BaseGeometry:
         self.name = name
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
         self.value = value
