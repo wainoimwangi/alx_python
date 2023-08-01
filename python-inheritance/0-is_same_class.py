158d04f0f004baddd221a5fd3f8f366c126fb2fb
wainoimwangi <wainoi413@gmail.com>
2023-08-01 10:22:24 +0300

    task 0

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
 
 
-
 """
 Write a function that returns True if the object is exactly
 an instance of the specified class ; otherwise False.
 
 Prototype: def is_same_class(obj, a_class):
 You are not allowed to import any module
+
+    
+Parameters:
+obj : any
+    The object to be checked for its class membership.
+a_class : class
+    The class that the object's membership is to be tested against.
+
+Method
+    is_same_class(obj, a_class)
 """
 
 
-class a_class:
+def is_same_class(obj, a_class)
     """
-    Represents a clas with an instance attribute 'a_class'.
-    That checks whether an instance of an object belongs to the 
-    specified same class.
-        
-    Parameters:
-    obj : any
-        The object to be checked for its class membership.
-    a_class : class
-        The class that the object's membership is to be tested against.
+    The is_same_class function returns True if the object obj 
+    is an instance of the specified class a_class, and False 
+    otherwise.
+    Returns:
+    True if the object is exactly an instance of the specified class; otherwise, False.
 
-    Method
-        is_same_class(obj, a_class)
     """
-    def is_same_class(obj, a_class)
-        """
-        The is_same_class function returns True if the object obj 
-        is an instance of the specified class a_class, and False 
-        otherwise.
-        Returns:
-        True if the object is exactly an instance of the specified class; otherwise, False.
-
-        """
-        return issubclass(type(obj))
+    return issubclass(type(obj))
