d584d078e491e48ba60749ef13f06cfe016e99e4
wainoimwangi <wainoi413@gmail.com>
2023-08-01 14:36:27 +0300

    task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -10,4 +10,11 @@ class BaseGeometry:
     """
     pass
 
-print(dir(BaseGeometry))
+# Get the list of attributes and methods using dir()
+all_members = dir(BaseGeometry)
+
+# Methods to exclude from the list
+methods_to_exclude = ["__init_subclass__"]
+
+# Filter the list to exclude the specified methods
+filtered_members = [member for member in all_members if member not in methods_to_exclude]
