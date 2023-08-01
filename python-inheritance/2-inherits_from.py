7bbcbc0e04b318a69ff21d90c326d8ee52ee06a4
wainoimwangi <wainoi413@gmail.com>
2023-08-01 12:25:11 +0300

    task 2

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Check if the given object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Parameters:
+    obj : any
+        The object to be checked for its class inheritance.
+    a_class : class
+        The class that the object's inheritance is to be tested against.
+
+    Returns:
+    bool
+        True if the object is an instance of a class that inherited (directly or indirectly)
+        from the specified class; otherwise, False.
+
+    Example:
+    >>> class Animal:
+    ...     pass
+    ...
+    >>> class Mammal(Animal):
+    ...     pass
+    ...
+    >>> class Dog(Mammal):
+    ...     pass
+    ...
+    >>> class Cat(Mammal):
+    ...     pass
+    ...
+    >>> doggo = Dog()
+    >>> inherits_from(doggo, Animal)
+    True
+    >>> kitty = Cat()
+    >>> inherits_from(kitty, Dog)
+    False
+    >>> inherits_from(kitty, Mammal)
+    True
+    """
+    return issubclass(obj, a_class) or type(obj) is a_class
