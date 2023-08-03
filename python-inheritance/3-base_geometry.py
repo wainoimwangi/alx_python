a12cc46b097bd62273ad3014c6e30b9594a49cb5
wainoimwangi <wainoi413@gmail.com>
2023-08-03 11:44:51 +0300

    task 6

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,3 +9,9 @@ class BaseGeometry:
     This is a base class
     """
     pass
+
+class MyNewClass:
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
