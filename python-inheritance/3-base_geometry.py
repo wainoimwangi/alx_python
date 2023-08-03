1e7b8284097e2a59996aa9093fc9701556e56d3c
wainoimwangi <wainoi413@gmail.com>
2023-08-03 22:36:06 +0300

    task 8

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,7 +4,19 @@
 """
 
 
-class BaseGeometry:
+class TypeMetaClass(type):
+    """
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+class BaseGeometry(metaclass=TypeMetaClass):
     """
     This is a base class
     """
