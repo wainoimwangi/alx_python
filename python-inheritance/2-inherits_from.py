ed9db2a8f4e944e687eb2baa48abeefc86c1b3b7
wainoimwangi <wainoi413@gmail.com>
2023-08-06 22:28:09 +0300

    corrections

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -40,4 +40,4 @@ def inherits_from(obj, a_class):
     >>> inherits_from(kitty, Mammal)
     True
     """
-    return isinstance(type(obj), a_class) and issubclass(type(obj), a_class) and type(obj) != a_class
+    return isinstance(type(obj), a_class) and issubclass(type(obj), a_class) and type(obj) == bool
