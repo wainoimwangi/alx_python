4573123f9fadcb3ba056fbc2ef24724af2c0e9cd
wainoimwangi <wainoi413@gmail.com>
2023-08-06 22:42:45 +0300

    corrections

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -29,23 +29,6 @@ class BaseGeometry(metaclass=TypeMetaClass):
 
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
 
-    def area(self):
-        """
-        Function that raise an exception with an error message
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Function validates whether the value is an integer and
-        is greater than 0, otherwise it raises an error
-        """
-        self.name = name
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        self.value = value
 
 class Rectangle(BaseGeometry):
     """
@@ -56,19 +39,12 @@ class Rectangle(BaseGeometry):
         function sets the values width and height and ensures
         """
         super().integer_validator("width", width)
-        self._Rectangle__width = width
+        self.__width = width
         super().integer_validator("height", height)
-        self._Rectangle__height = height
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
     
     def __str__(self):
         return f"[Rectangle] {self.__width}/{self.__height}"
-
-
-class Square(Rectangle):
-    """
-    This is a sub-class of the Rectangle class
-    """
-    pass
