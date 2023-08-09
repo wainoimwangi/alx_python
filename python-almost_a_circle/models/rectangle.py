de5c3203d66fe4625d71a7d45c9986f442ed9e4a
wainoimwangi <wainoi413@gmail.com>
2023-08-10 00:25:46 +0300

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,7 +80,7 @@ class Rectangle(Base):
         """
         if value is not int:
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -99,6 +99,6 @@ class Rectangle(Base):
         """
         if value is not int:
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
