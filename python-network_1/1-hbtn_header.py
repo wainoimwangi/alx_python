6172899651e032b836466074ce5dbacdecc5120b
wainoimwangi <wainoi413@gmail.com>
2023-08-11 22:18:04 +0300

    task 0

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This isaa Python script that takes in a URL, sends a request to the URL 
+and displays the value of the variable X-Request-Id in the response header
+
+You must use the packages requests and sys
+The value of this variable is different for each request
+"""
+import requests
+import sys
+
+def check_status(url):
+    """
+    Fetches the X-Request-Id variable from the response header of the given URL.
+    """
+    try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    x_request_id = response.headers.get('X-Request-Id')
+
+    if x_request_id:
+        print("X-Request-Id:", x_request_id)
+    else:
+        print("X-Request-Id not found in response header.")
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", e)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <url>")
+    else:
+        url = sys.argv[1]
+        fetch_and_display_request_id(url)
