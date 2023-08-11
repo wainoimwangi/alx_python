0a685643b2f9805033ad04c0ea313cb28da41c41
wainoimwangi <wainoi413@gmail.com>
2023-08-11 22:49:37 +0300

    task 2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This is a Python script that takes in a URL and an email address, 
+sends a POST request to the passed URL with the email as a parameter, 
+and finally displays the body of the response.
+
+The email must be sent in the variable email
+Uses the packages requests and sys
+"""
+import requests
+import sys
+
+def check_email(url):
+    """
+    Sends a POST request to the given URL with the email as a parameter
+    and displays the response body.
+
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to include in the request.
+    """
+    try:
+        payload = {'email': email}
+        response = requests.get(url, data=payload)
+        response.raise_for_status()
+
+        context = response.text
+
+        print("Your email is: {}".format(context))
+    
+    except requests.exceptions.RequestException as e:
+        print("Error occured:", e)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    check_email(url, email)
+
+
+        
