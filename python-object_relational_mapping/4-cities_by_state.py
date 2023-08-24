c4fe46017a617814a8278f29b49472a9e91cf64d
root <root@DESKTOP-HBU0M34>
2023-08-24 23:23:21 +0300

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,10 @@ if __name__ =="__main__":
     """
     execute the sql query
     """
-    cursor.execute("SELECT * FROM cities\
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+                    FROM cities\
+                    INNER JOIN states\
+                    ON cities.state_id = states.id\
                     ORDER BY cities.id ASC")
     """
     fetch and display results
@@ -32,4 +35,3 @@ if __name__ =="__main__":
     """
     cursor.close()
     db.close()
-    
