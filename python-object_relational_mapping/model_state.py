0111129ead91d18a59121a085b4300f5436c8331
root <root@DESKTOP-HBU0M34>
2023-08-25 16:30:12 +0300

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,15 +3,15 @@
 python script that contains the class definition of a State
 and an instance Base = declarative_base()
 '''
+# import sqlalchemy modules
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+# define the declarative base class
 Base = declarative_base()
 
 
-'''
-state class that inherits from base class
-'''
+# create a class state that inherits from base class
 class State(Base):
     '''
     State class that inherits from the base class.
@@ -19,11 +19,9 @@ class State(Base):
         id- auto-generated, unique integer, not null and a primary key
         name- string of length 128 character
     '''
+    # define the table name
     __table__ = "states"
-
+    # set the column of the states table
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-    def __init__(self, name):
-        self.name = name
