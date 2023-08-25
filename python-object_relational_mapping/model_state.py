9816370e6f1ada26e9f836911a390ae579cbc558
root <root@DESKTOP-HBU0M34>
2023-08-25 15:48:51 +0300

    task model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,7 +15,7 @@ class State(Base):
         id- auto-generated, unique integer, not null and a primary key
         name- string of length 128 character
     """
-    __table__ = 'State'
+    __table__ = 'states'
 
     id = column(integer, primary_key=True, nullable=False, autoincrement=True)
     name = column(string(128), nullable=False)
