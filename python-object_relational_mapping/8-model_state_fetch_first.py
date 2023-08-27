e34182d53f7300b0eac3a80de068d7d5d54ff7bc
root <root@DESKTOP-HBU0M34>
2023-08-27 22:00:14 +0300

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,9 +7,13 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ = "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+                           pool_pre_ping=True)
 
     session_class = sessionmaker(bind=engine)
     session = session_class()
