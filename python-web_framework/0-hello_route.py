5d8979bb97bd8766634cac663b8eab71fe73f518
root <root@DESKTOP-HBU0M34>
2023-08-31 21:16:50 +0300

    task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slasher=False)
 def hello():
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
