d370959ba17be73eb74bd75b34048e9a41ae5b2e
root <root@DESKTOP-HBU0M34>
2023-08-31 21:33:09 +0300

    task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -9,13 +9,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # define route for the homepage
-@app.route("/", strict_slasher=False)
-def hello():
+@app.route('/', strict_slasher=False)
+def hello_hbnb():
     """
     a function that displays hello hbnb
     """
     return "Hello HBNB!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # run the flask developement server
     app.run(host='0.0.0.0', port='5000')
