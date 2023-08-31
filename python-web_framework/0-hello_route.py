ecaa672d19e9b50ad5bd7f2aab7c2d2ad1dc2841
root <root@DESKTOP-HBU0M34>
2023-08-31 21:26:23 +0300

    task 0

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -4,11 +4,17 @@ Script that starts a flask web application
 """
 from flask import Flask
 
+# create a flask web application instance
 app = Flask(__name__)
 
+# define route for the homepage
 @app.route("/", strict_slasher=False)
 def hello():
+    """
+    a function that displays hello hbnb
+    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
+    # run the flask developement server
     app.run(host='0.0.0.0', port=5000)
